kostyll/gajim

commit 246d36d9322cba22798aab255587082e03367330

Remove useless import and fix GPG private key dialog.
Do not fail when no key is selected.


git-svn-id: svn://svn.gajim.org/gajim/trunk@9209 c4d4e39b-2f5f-9a77-1c94-36969bb79148

src/dialogs.py
@@ -351,10 +351,10 @@ class ChooseGPGKeyDialog:
 
 	def run(self):
 		rep = self.window.run()
-		if rep == gtk.RESPONSE_OK:
-			selection = self.keys_treeview.get_selection()
-			(model, iter) = selection.get_selected()
-			keyID = [ model[iter][0].decode('utf-8'),
+		selection = self.keys_treeview.get_selection()
+		(model, iter) = selection.get_selected()
+		if iter and rep == gtk.RESPONSE_OK:
+				keyID = [ model[iter][0].decode('utf-8'),
 				model[iter][1].decode('utf-8') ]
 		else:
 			keyID = None

src/config.py
@@ -1362,8 +1362,6 @@ class AccountsWindow:
 		self.accounts_treeview = self.xml.get_widget('accounts_treeview')
 		self.remove_button = self.xml.get_widget('remove_button')
 		self.rename_button = self.xml.get_widget('rename_button')
-		#FIXME: I don't understand why this import is necessary
-		import os
 		path_to_kbd_input_img = os.path.join(gajim.DATA_DIR, 'pixmaps',
 			'kbd_input.png')
 		img = self.xml.get_widget('rename_image')

commit 1732328bf65cea686aa8f71840e4630d1c06ce62

Fix indent error, introduced in r9209

src/dialogs.py
@@ -354,7 +354,7 @@ class ChooseGPGKeyDialog:
 		selection = self.keys_treeview.get_selection()
 		(model, iter) = selection.get_selected()
 		if iter and rep == gtk.RESPONSE_OK:
-				keyID = [ model[iter][0].decode('utf-8'),
+			keyID = [ model[iter][0].decode('utf-8'),
 				model[iter][1].decode('utf-8') ]
 		else:
 			keyID = None